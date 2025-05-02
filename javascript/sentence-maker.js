9417319f3cd3604f4b1780fee7bf3375ac418972
agpereira94 <pereirinha94@gmail.com>
2025-05-02 07:54:11 +0200

    Created folder for JS in fCC + 3 exercises

---
+++ javascript/sentence-maker.js
@@ -0,0 +1,33 @@
+let adjective;
+let noun;
+let verb;
+let place;
+let adjective2;
+let noun2;
+
+adjective = "flamboyant";
+noun = "dog";
+verb = "raining";
+place = "thermal waters complex";
+adjective2 = "short";
+noun2 = "glass";
+
+let firstStory;
+
+firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("First story: " + firstStory);
+
+let secondStory
+
+adjective = "gorgeous";
+noun = "priest";
+verb = "rocking";
+place = "St. Peter's Cathedral";
+adjective2 = "slimy";
+noun2 = "plants";
+
+secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+console.log("Second story: " + secondStory);
+
---
+++ javascript/trivia-bot.js
@@ -0,0 +1,27 @@
+console.log("Hello! I'm your coding fun fact guide!");
+
+let botName;
+let botLocation;
+let favoriteLanguage;
+
+botName = "Zeddy Freddy";
+botLocation = "Pluto";
+favoriteLanguage = "JavaScript";
+
+console.log("My name is " + botName + " and I live on " + botLocation + ".");
+
+console.log("My favorite programming language is " + favoriteLanguage + ".");
+
+let codingFact = favoriteLanguage + " shares the name with a Star Wars character. Kind of, anyway!";
+
+console.log(codingFact);
+
+codingFact = "After all, " + favoriteLanguage + " is as science fiction as Star Wars.";
+
+console.log(codingFact);
+
+codingFact = "I'm bored of sharing facts about " + favoriteLanguage + " with you.";
+
+console.log(codingFact);
+
+console.log("It was fun sharing these facts with you. Goodbye! - " + botName + " from " + botLocation ".");
